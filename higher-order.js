75af35b4037be72e55ba2da5eff744bf77d1578c
tenndawg <nickolas.tenney@gmail.com>
2017-08-23 19:10:56 -0600

    higher order questions

--- higher-order.js
+++ higher-order.js
@@ -9,7 +9,7 @@
 
 const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(e, i, arr){return num % 2 === 0;});
 
 // Below we have an array of prices.  We want to calculate their totals after
 // We want to use a map function on the prices to calculate a new array of
@@ -24,7 +24,7 @@ let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices // = prices.map(function(e, i, arr){for (var i = 0; i < arr.length; i++){var tax = []; tax.push(arr[i]*.07+arr[i]}}));
 
 
 
