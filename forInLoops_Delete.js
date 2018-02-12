acb77af7b812ea9668d1a5b3a4f08a29cc49833d
tenndawg <nickolas.tenney@gmail.com>
2018-02-12 14:47:42 -0700

    working on the last one

--- forInLoops_Delete.js
+++ forInLoops_Delete.js
@@ -7,23 +7,23 @@
 
 // In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 
- var values = {
-   one: 'These',
-   two: ' are',
-   three: ' the',
-   four: ' property',
-   five: ' values.'
- } 
+//  var values = {
+//    one: 'These',
+//    two: ' are',
+//    three: ' the',
+//    four: ' property',
+//    five: ' values.'
+//  } 
 
- for(var key in values) {
-   console.log(values[key])
- }
+//  for(var key in values) {
+//    console.log(values[key])
+//  }
 
 // In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 
- for(var key in values) {
-   console.log(key)
- }
+//  for(var key in values) {
+//    console.log(key)
+//  }
 
 
 // ========================
@@ -31,12 +31,13 @@
 
 // Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 
-function showValues( obj ) {
+function showValues(obj) {
+  var final = [];
   for (var key in obj) {
-    console.log("".concat(obj[key]));  
+    final = final.concat(obj[key]);
   }
-  return obj;
-}
+  return final.join('');
+};
 
 
 // ========================
@@ -47,7 +48,7 @@ function showValues( obj ) {
 function greaterThan10(obj) {
 	for (var key in obj) {
 		if (obj[key] > 10) {
-			obj[key] = obj[key]+ 10);
+			obj[key] = 0;
 		}
 	}
 	return obj;
@@ -62,7 +63,7 @@ function greaterThan10(obj) {
 
 function double(obj) {
 	for (var key in obj) {
-		obj[key] * 2;
+	obj[key] =	obj[key] * 2;
 	}
 	return obj;
 }
@@ -76,6 +77,15 @@ function double(obj) {
 
 // CODE HERE
 
+function secrets(obj) {
+  var nada = '';
+  for (var key in obj) {
+    if (key == 'sh') {
+      nada.push(obj[key]);
+    }
+  }
+  return nada;
+}
 
 // ========================
 
